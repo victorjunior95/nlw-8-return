db67e81b335d147ef7113e1bac4c54956bf6fe22
Victor Junior da Silva Ramos <victorjr.muchacho@gmail.com>
2022-05-03 09:52:56 -0300

    Stage 1 completed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      // criando uma extensão de temas de cor
+      colors: {
+        brand: {
+          500: "#8257e6"
+        }
+      }
+    }
+  },
+  plugins: [],
+}
